02156b081d8fac91fd89e545e9d30ba8719c2bdf
Fridolin@top <fridolinhaag@yahoo.de>
2014-01-09 14:38:22 +0100

    Add to git

---
+++ scripts/read.R
@@ -0,0 +1,43 @@
+######### SATELLITE PRECIPITATION ESTIMATES – ASSESSING FEASABILITY FOR KAPUAS CATCHMENT ######
+
+## read.R
+## Reads in the data
+
+#### SET UP ####
+  # call libraries
+  library("SDMTools")
+  library("raster")
+  
+  source("scripts/functions.R")
+
+####PERSIANN DATA####
+  # reads in daily persiann data and converts it to raster object.
+  # PERSIANN data is in ESRI asc format, but correctly subsetted
+  # therefore asc data is read and then converted to raster objects
+  # 2001-2012 start date: 2001-01-01
+  fpath="input/PERSIANN"
+  files=list.files(path=fpath, pattern="*.asc", 
+                   recursive=TRUE, include.dirs=TRUE, full.names=TRUE)
+  persiann.rlist=list() #initialize
+  for (i in 1:length(files)) {
+    asc.tmp    =read.asc(file=files[i])
+    raster.tmp =raster(asc.tmp)
+    persiann.rlist[i]=raster.tmp
+  }  #there is an error for unknown reasons, but it runs through, takes time however
+
+  persiann=brick(persiann.rlist)
+  rm(i, asc.tmp, raster.tmp, persiann.rlist)
+  str(persiann)
+  names(persiann)
+  plot(persiann[[4]])
+
+#### CMORPH DATA ####
+  library("raster")
+  cmorph<-brick("input/CMORPH/cmorph.nc", varname="cmorph")
+  str(cmorph)
+  names(cmorph)    
+  plot(cmorph[[4]])
+
+
+
+####### END read.R #####
