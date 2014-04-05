590d3196408c10847e60435974e1aed1433966aa
Fridolin@top <fridolinhaag@yahoo.de>
2014-04-05 11:55:07 +0200

    * spatial part comsetics

--- scripts/spatial_aggregate.R
+++ scripts/spatial_aggregate.R
@@ -1,5 +1,8 @@
-#### Spatial Aggregation of raster time series ####
-
+###### SPATIO-TEMPORAL RAINFALL PATTERNS IN KAPUAS BASIN ######
+	### ASSESSING USEFULNESS OF SATELLITE PRECIPITATION ESTIMATES ###
+	### SPATIAL COMPARISON ####
+	
+#### A: SUBCATCHMENT AVERAGES ####
 #### SET UP ####
   library("sp")
   library("maptools")
@@ -8,13 +11,16 @@
   library("rts")
   
   projection=CRS(projection(trmm))
-### Load shapefiles ###
+###
+
+#### Load shapefiles ####
   shps=list(
       readShapePoly(fn="input//gis//tayan-shp//tayan-shp.shp", proj4string=projection),
       readShapePoly(fn="input//gis//sekayam-shp//sekayam-shp.shp", proj4string=projection),
         readShapePoly(fn="input//gis//belitang-shp/belitang.shp", proj4string=projection)
         )
-  extents=lapply(shps, function(x) extent(x)+0.3)
+  extents=lapply(shps, function(x) extent(x)+0.3) # get extents of subcatchments
+###
 
 #### Spatial Subsetting ####
 # 1.Crop area
@@ -49,10 +55,11 @@
   mask.cmorph   <-mask.area(da.cmorph, shp=shps)
   mask.trmm     <-mask.area(da.trmm, shp=shps)
   rm(da.persiann, da.cmorph, da.trmm)
+###
 
 #### Convert to raster time series and aggregate####
 # PERSIANN
-  # na.rm is set to TRUE!!
+  # na.rm is set to TRUE!
   persiann.d_ts=lapply(mask.persiann, rts, time=time.d)
   persiann.m_ts=lapply(persiann.d_ts, apply.monthly, mean,na.rm=TRUE)
   persiann.y_ts=lapply(persiann.d_ts, apply.yearly, mean,na.rm=TRUE)
@@ -66,8 +73,10 @@
   trmm.y_ts=lapply(trmm.d_ts, apply.yearly, mean)
 
   rm(mask.persiann, mask.cmorph, mask.trmm )
+###
 
-#### Make time series of catchment wide means values ####
+#### CATCHMENT MEANS ####
+## Make time series of catchment wide means values
 ## They can be compared to IDW spatial interpolations of mean values for the whole catchment
 # PERSIANN
   persiann.sp.d_ts=lapply(persiann.d_ts, function(x) zoo(cellStats(x@raster,stat=mean), order.by=time.d))
@@ -82,45 +91,63 @@
   trmm.sp.m_ts=lapply(trmm.m_ts, function(x) zoo(cellStats(x@raster,stat=mean), order.by=time.m))
   trmm.sp.y_ts=lapply(trmm.y_ts, function(x) zoo(cellStats(x@raster,stat=mean), order.by=time.y))
 ###
+### END A: SUBCATCHMENT AVERAGES ###
 
-#### Part II: AGGREGATE ON MAP LEVEL####
+#### B: AGGREGATE ON MAP LEVEL ####
+####  SET UP ####
   library("maptools")
   library("raster")
   library("rts")
- ### Load shapefiles ###
+###
+
+#### Load shapefiles ####
   projection=CRS(projection(cmorph))
   subcatch_shp <-readShapePoly(fn="input/gis/subcatchments/subcatchments.shp", IDvar="catchment", proj4string=projection)
   kapuas_shp  <-readShapePoly(fn="input/gis/kapuas-basin//kapuas-basin.shp", IDvar="DN", proj4string=projection)
   stations_shp<-readShapePoints("input/gis/stationmap/stationmap.shp")
   stations_shp=stations_shp[c(-8,-9,-13),] # exclude SGU04, SGU19, STG03
 ###
+
 #### Subsetting ####
   ext=extent(subcatch_shp)+0.4
+  # Crop:
   crop.cmorph=crop(cmorph, ext)
   crop.persiann=crop(persiann, ext)
   crop.trmm=crop(trmm, ext)
   srfe_subcatch=list("CMORPH"=crop.cmorph, "PERSIANN"=crop.persiann, "TRMM"=crop.trmm)
   rm(crop.cmorph, crop.persiann, crop.trmm)
+  # Disaggregate:
   srfe_subcatch.da=lapply(srfe_subcatch, disaggregate, fact=5) 
   rm(srfe_subcatch) 
-  # set again the extent 
+#  # set again the extent 
   extnt=extent(109.75,111.75,-0.1,1.25)
   srfe_subcatch.da.recrop=lapply(srfe_subcatch.da, crop, extnt)
+  # Mask:
   mask.srfe_subcatch <-lapply(srfe_subcatch.da.recrop, raster::mask, mask=subcatch_shp)
-   rm(srfe_subcatch.da,srfe_subcatch.da.recrop)
+  rm(srfe_subcatch.da,srfe_subcatch.da.recrop)
+###
+   
 #### AGGREGATING ####
+# Daily, monthly, yearly
+	# Make time series
   srfe_subcatch.d_ts=lapply(mask.srfe_subcatch, rts, time=time.d)
+  # Aggretate to monthly and yearly
   srfe_subcatch.m_ts=lapply(srfe_subcatch.d_ts, apply.monthly, mean)
-  # srfe_subcatch.y_ts=lapply(cmorph.d_ts, apply.yearly, mean)
+#  srfe_subcatch.y_ts=lapply(cmorph.d_ts, apply.yearly, mean)
   rm(srfe_subcatch.d_ts)
-### by month mean ###
+  
+# By month mean
+	# setZ
   mask.srfe_subcatch.Z=lapply(mask.srfe_subcatch,setZ, z=time.d, name="time")
   mon.fac <- format.Date(time.d,format="%m")
   mon.fac <- factor(mon.fac)
+  # aggregate
   subcatch.srfe.bymonth=lapply(mask.srfe_subcatch.Z, zApply, by=mon.fac, fun=mean, name='months')
   #names(subcatch.idw.bymonth)=format.Date(datesx,format="%b")[1:12]
   rm(mask.srfe_subcatch.Z)
-### Long term mean ###
+  
+# Long term mean
   subcatch.srfe.ov.av=lapply(srfe_subcatch.m_ts, period.apply, 144,mean, na.rm=TRUE) #because it's 144 month
-### END AGGREGATION PART II ###
-#### END ####
+###
+### END B: AGGREGATE ON MAP LEVEL ###
+###### END spatial_aggregate.R ######
--- scripts/spatial_compare.R
+++ scripts/spatial_compare.R
@@ -1,21 +1,28 @@
-#### Spatial Comparison ####
+###### SPATIO-TEMPORAL RAINFALL PATTERNS IN KAPUAS BASIN ######
+	### ASSESSING USEFULNESS OF SATELLITE PRECIPITATION ESTIMATES ###
+	### SPATIAL COMPARISON ####
 
+#### SET UP ####
 library("hydroTSM")
 source("scripts//functions.R")
+###
+
 #### PART I COMPARE WITH INTERPOLATED CATCHMENT TOTALS####
   #### Read in interpolated data ####
-  #daily
+  # daily
     idw.d=read.csv("input/interpolation_results//subcatch_d_idw.csv")
     idw.d_ts=list(zoo(idw.d$PTayan, order.by=time.d),zoo(idw.d$PSekayam, order.by=time.d),zoo(idw.d$PBelitang, order.by=time.d) )
     names(idw.d_ts)=c("Tayan", "Sekayam", "Belitang")
-  #monthly
+  # monthly
     idw.m=read.csv("input/interpolation_results//subcatch_m_idw.csv")
     idw.m_ts=list(zoo(idw.m$PTayan, order.by=time.m),zoo(idw.m$PSekayam, order.by=time.m),zoo(idw.m$PBelitang, order.by=time.m) )
     names(idw.m_ts)=c("Tayan", "Sekayam", "Belitang")
   # aggregate monthly to yearly
     idw.y_ts=lapply(idw.m_ts, monthly2annual, mean)
+  ###
   
-  #### Comparative plots ####
+  #### COMPARATIVE SCATTERPLOTS ####
+  # with scatter.grid function
   fpath="output/spatial_comparison"
   dir.create(fpath)
   # Daily
@@ -47,94 +54,98 @@ source("scripts//functions.R")
     scatter.grid(yearly.sp.comp, xylim=c(6,13), leftsidetext="IDW interpolation (mm/day)")
     dev.off()
   ###
-  #### Correlation Coefficients ####
+  
+  #### PEARSON CORRELATION COEFFICIENTS ####
   fpath="output/spatial_comparison"
   write.csv(pairw.corr(daily.sp.comp), file=paste(fpath,"/daily_cor.csv", sep=""))
   write.csv(pairw.corr(monthly.sp.comp), file=paste(fpath,"/monthly_cor.csv", sep=""))
   write.csv(pairw.corr(yearly.sp.comp), file=paste(fpath,"/yearly_cor.csv", sep=""))
   ###
 ### END PART I ###
+
 #### PART II: COMPARE ON MAP LEVEL ####
-## Load interpolation data
+	#### Load interpolation data ####
   load(file="input//interpolation_results/subcatch.idw.bymonth")
   load(file="input//interpolation_results/subcatch.idw.ov.av")
-  #change extents to match the ones of the satellite imagery
+  # change extents to match the ones of the satellite imagery
   subcatch.idw.bymonth=extend(x=subcatch.idw.bymonth, y=extnt)
   subcatch.idw.ov.av=extend(x=subcatch.idw.ov.av@raster, y=extnt)
-###
-#### COMPARE MONTHLY VALUES OF PEAK RAINY AND DRY SEASON ####
-fpath="output/spatial_comparison/month"
-dir.create(fpath)
-## appropirate scaling
+	###
+	
+	#### PLOT MONTHLY VALUES OF PEAK RAINY AND PEAK DRY SEASON ####
+	fpath="output/spatial_comparison/month"
+	dir.create(fpath)
+#		## appropirate scaling
 #   levelplot(... at=scaling,...)
 #   lapply(subcatch.srfe.bymonth, function(x) cellStats(x, max))
-#    cellStats(subcatch.idw.bymonth[[11]], min)
+#   cellStats(subcatch.idw.bymonth[[11]], min)
 #   lapply(subcatch.srfe.bymonth, function(x) cellStats(x[[8]], max))
 #   cellStats(subcatch.idw.bymonth[[8]], max)
-  scaling <- seq(2.9, 13.6, (2/3))
-  # Plot January values  
-name=paste(fpath,"/nov_idw.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.idw.bymonth[[11]], at=scaling)
-  dev.off()
-name=paste(fpath,"/nov_cmorph.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.srfe.bymonth[[1]][[11]], at=scaling)
-dev.off()
-name=paste(fpath,"/nov_persiann.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-      llplot(subcatch.srfe.bymonth[[2]][[11]],at=scaling)
-dev.off()
-name=paste(fpath,"/nov_trmm3b42.svg", sep="")
-svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.srfe.bymonth[[3]][[11]],at=scaling)
-dev.off()
-# August values
-fpath="output/spatial_comparison/month"
-name=paste(fpath,"/aug_idw.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.idw.bymonth[[8]],at=scaling)
-dev.off()
-name=paste(fpath,"/aug_cmorph.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.srfe.bymonth[[1]][[8]],at=scaling)
-dev.off()
-name=paste(fpath,"/aug_persiann.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.srfe.bymonth[[2]][[8]],at=scaling)
-dev.off()
-name=paste(fpath,"/aug_trmm3b42.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.srfe.bymonth[[3]][[8]],at=scaling)
-dev.off()
+		scaling <- seq(2.9, 13.6, (2/3))
+	# Plot January values  
+	name=paste(fpath,"/nov_idw.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.idw.bymonth[[11]], at=scaling)
+		dev.off()
+	name=paste(fpath,"/nov_cmorph.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.srfe.bymonth[[1]][[11]], at=scaling)
+	dev.off()
+	name=paste(fpath,"/nov_persiann.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		    llplot(subcatch.srfe.bymonth[[2]][[11]],at=scaling)
+	dev.off()
+	name=paste(fpath,"/nov_trmm3b42.svg", sep="")
+	svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.srfe.bymonth[[3]][[11]],at=scaling)
+	dev.off()
+	# Plot August values
+	name=paste(fpath,"/aug_idw.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.idw.bymonth[[8]],at=scaling)
+	dev.off()
+	name=paste(fpath,"/aug_cmorph.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.srfe.bymonth[[1]][[8]],at=scaling)
+	dev.off()
+	name=paste(fpath,"/aug_persiann.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.srfe.bymonth[[2]][[8]],at=scaling)
+	dev.off()
+	name=paste(fpath,"/aug_trmm3b42.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.srfe.bymonth[[3]][[8]],at=scaling)
+	dev.off()
+	###
 
-rm(fpath)
-#### Plot long term averages ###
-fpath="output/spatial_comparison/lta"
-dir.create(fpath)
-  # SCALING
+	#### PLOT LONG TERM MEANS ###
+	fpath="output/spatial_comparison/lta"
+	dir.create(fpath)
+#		## appropirate scaling
 #   lapply(subcatch.srfe.ov.av, function(x) cellStats(x@raster, max))
 #   cellStats(subcatch.idw.ov.av, min)
   scaling <- seq(7.3, 11.5, 0.2)
+  # Plot
+	name=paste(fpath,"/lta_idw.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.idw.ov.av, at=scaling)
+	dev.off()
+	name=paste(fpath,"/lta_cmorph.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.srfe.ov.av[[1]]@raster, at=scaling)
+	dev.off()
+	name=paste(fpath,"/lta_persiann.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.srfe.ov.av[[2]]@raster, at=scaling)
+	dev.off()
+	name=paste(fpath,"/lta_trmm3b42.svg", sep="")
+		svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
+		  llplot(subcatch.srfe.ov.av[[3]]@raster, at=scaling)
+	dev.off()
+	###
+### END PART II ###
 
-name=paste(fpath,"/lta_idw.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.idw.ov.av, at=scaling)
-dev.off()
-name=paste(fpath,"/lta_cmorph.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.srfe.ov.av[[1]]@raster, at=scaling)
-dev.off()
-name=paste(fpath,"/lta_persiann.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.srfe.ov.av[[2]]@raster, at=scaling)
-dev.off()
-name=paste(fpath,"/lta_trmm3b42.svg", sep="")
-  svg(filename=name, pointsize = 11, width=8, height=7, family="Lato")
-    llplot(subcatch.srfe.ov.av[[3]]@raster, at=scaling)
-dev.off()
-###
 #### CLEAN UP ####
 rm(fpath)
 ###
-#### END ####
+###### END spatial_compare.R ######
